f97d45f97ce5fefc5f29ba4200e58b5e4fb77a89
ntabucejo <ntabucejo@gmail.com>
2022-11-10 21:44:26 +0800

    initial: api

---
+++ app/main.ts
@@ -0,0 +1,13 @@
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT;
+
+app.get("/", (request: Request, response: Response) => {
+  response.status(200).send({ message: "Hello" });
+});
+
+app.listen(port);
